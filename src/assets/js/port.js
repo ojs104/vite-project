98b52c01a4763e3cdb118c643638c8f160160b2e
ojs104 <kmdojs@gmail.com>
2023-11-06 13:55:38 +0900

    footer

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,3 +1,23 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
 export function port() {
-    console.log("port");
+    gsap.registerPlugin(ScrollTrigger);
+    const horSection = gsap.utils.toArray(".port__item");
+
+    gsap.to(horSection, {
+        xPercent: -120 * (horSection.length - 1),
+        ease: "none",
+        scrollTrigger: { // "ScrollTrigger"가 올바른 대소문자로 수정됨
+          trigger: "#port",
+          start: "top top", // start 옵션의 값도 수정됨
+          end: "+=3000",
+          pin: true,
+          scrub: 1,
+          markers: true,
+          invalidateOnRefresh: true,
+          anticipatePin: 1
+        }
+      });
 }
+
