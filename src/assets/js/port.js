ee27c4eff000f1bc3747afbdfddfd1e05f267ffa
ojs104 <kmdojs@gmail.com>
2023-11-09 14:12:43 +0900

    footer 수정

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -2,22 +2,22 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
 export function port() {
-    gsap.registerPlugin(ScrollTrigger);
-    const horSection = gsap.utils.toArray(".port__item");
+  gsap.registerPlugin(ScrollTrigger);
+  const horSection = gsap.utils.toArray(".port__item");
 
-    gsap.to(horSection, {
-        xPercent: -120 * (horSection.length - 1),
-        ease: "none",
-        scrollTrigger: { // "ScrollTrigger"가 올바른 대소문자로 수정됨
-          trigger: "#port",
-          start: "top top", // start 옵션의 값도 수정됨
-          end: "+=3000",
-          pin: true,
-          scrub: 1,
-          markers: true,
-          invalidateOnRefresh: true,
-          anticipatePin: 1
-        }
-      });
+  gsap.to(horSection, {
+    xPercent: -120 * (horSection.length - 1),
+    ease: "none",
+    scrollTrigger: { // "ScrollTrigger"가 올바른 대소문자로 수정됨
+      trigger: "#port",
+      start: "top 56px", // start 옵션의 값도 수정됨
+      end: "+=3000",
+      pin: true,
+      scrub: 1,
+      markers: true,
+      invalidateOnRefresh: true,
+      anticipatePin: 1
+    }
+  });
 }
 
